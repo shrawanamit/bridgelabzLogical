17fd50925c65c58bb76019d82ffc9f9399bd3dd1
shrawanamit <amitkumar06111@gmail.com>
2020-04-19 20:22:01 +0530

    second commit three input use case 2,3,4,5

--- threeInput.sh
+++ threeInput.sh
@@ -3,3 +3,8 @@ echo "enter three integer values"
 read a
 read b
 read c
+compute=$(($a + $b * $c))
+compute1=$(($a * $b + $c))
+compute2=$(($c + $a / $b))
+compute3=$(($a % $b + $c))
+echo $compute $compute1 $compute2 $compute3
