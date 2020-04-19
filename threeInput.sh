11470968f60b593fca61fe7ffe224574d044ba53
shrawanamit <amitkumar06111@gmail.com>
2020-04-19 22:24:11 +0530

    third commit to three input use case 6

--- threeInput.sh
+++ threeInput.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A  dictionary
 echo "enter three integer values"
 read a
 read b
@@ -7,4 +8,8 @@ compute=$(($a + $b * $c))
 compute1=$(($a * $b + $c))
 compute2=$(($c + $a / $b))
 compute3=$(($a % $b + $c))
-echo $compute $compute1 $compute2 $compute3
+dictionary[key]=$compute
+dictionary[key1]=$compute1
+dictionary[key2]=$compute2
+dictionary[key3]=$compute3
+echo ${dictionary[@]}
